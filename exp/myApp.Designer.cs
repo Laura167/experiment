94c2eea006852ca838ce7117c1a1a028e68ac7db
laura <lauraslosberga80@gmail.com>
2020-04-03 17:12:40 +0300

    version 17:05

--- exp/myApp.Designer.cs
+++ exp/myApp.Designer.cs
@@ -34,30 +34,32 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.PeachPuff;
             this.button1.Location = new System.Drawing.Point(50, 56);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.PeachPuff;
             this.button2.Location = new System.Drawing.Point(50, 112);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // myApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "myApp";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
